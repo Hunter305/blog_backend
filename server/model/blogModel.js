38f220dcbebcd041ac2a67bdd6308b1bca93f56e
Hunter305 <avinash8169@gmail.com>
2023-10-20 10:56:52 +0530

    created model

---
+++ server/model/blogModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+  blog: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+export default Blog;
