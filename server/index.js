8f6ce84d6a5a106d731a2427a0ef1fbcc505c256
Jagan0901 <110884222+Jagan0901@users.noreply.github.com>
2023-10-20 17:26:52 +0530

    jagan initial commit

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import db from "./config/db.js";
+import { blogRouter } from "./routes/blogRouter.js";
 
 const port = process.env.PORT;
-const app = express();
+const app = express(); 
 
+app.use(express.json());
+ 
 db();
 // just adding comments
 app.get("/", (req, res) => {
-  res.send("app is running");
+  res.send("app is running"); 
 });
 
+app.use("/api/blog",blogRouter)
+
 app.listen(port, () => {
-  console.log(`app is running on port ${port}`);
+  console.log(`app is running on port ${port}`); 
 });
