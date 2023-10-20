f7f0977e8ddbcd26ae4f701f348512fed52b4a0b
Hunter305 <avinash8169@gmail.com>
2023-10-20 11:40:59 +0530

    added a error middleware and asynchandler

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import db from "./config/db.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT;
 const app = express();
@@ -11,6 +12,9 @@ db();
 app.get("/", (req, res) => {
   res.send("app is running");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
