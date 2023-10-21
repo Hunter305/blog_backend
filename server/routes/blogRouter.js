abc575a07ee7d7bca19b6cbc3b914210d2d784c4
Hunter305 <avinash8169@gmail.com>
2023-10-21 10:43:57 +0530

    added authentication and authorization

--- server/routes/blogRouter.js
+++ server/routes/blogRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { addBlog, allBlog, updateBlog,deleteBlog } from "../controllers/blogControllers.js";
+import {
+  addBlog,
+  allBlog,
+  updateBlog,
+  deleteBlog,
+} from "../controller/blogController.js";
 
 const router = express.Router();
 
@@ -7,7 +12,5 @@ router.route("/create").post(addBlog);
 router.route("/").get(allBlog);
 router.route("/edit/:id").put(updateBlog);
 router.route("/:id").delete(deleteBlog);
-
-
 
 export const blogRouter = router;
