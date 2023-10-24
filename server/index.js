3a22acdc35da6a417605da9c4d393e1bb49e73cc
Hunter305 <avinash8169@gmail.com>
2023-10-24 15:21:02 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -3,38 +3,34 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 import db from "./config/db.js";
+import cors from "cors";
 
 import { blogRouter } from "./routes/blogRouter.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import userRoute from "./routes/userRoutes.js";
 
-
 const port = process.env.PORT;
-const app = express(); 
+const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-
- 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 db();
 // just adding comments
 app.get("/", (req, res) => {
-  res.send("app is running"); 
+  res.send("app is running");
 });
 
-
-app.use("/api/blog",blogRouter)
+app.use("/api/blog", blogRouter);
 
 app.use("/api/users", userRoute);
 
 app.use(notFound);
 app.use(errorHandler);
 
-
 app.listen(port, () => {
-  console.log(`app is running on port ${port}`); 
+  console.log(`app is running on port ${port}`);
 });
